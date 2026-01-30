10e01a66693bde4b372791d5abb23b6b0146aac4
wu <wu@.com>
2026-01-30 12:55:51 +0800

    Logs-filips123/contenthashpy

--- setup.py
+++ setup.py
@@ -1,72 +1,63 @@
-import re
+from setuptools import setup
+from pathlib import Path
+import os
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-with open("loguru/__init__.py", "r") as file:
-    regex_version = r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]'
-    version = re.search(regex_version, file.read(), re.MULTILINE).group(1)
-
-with open("README.rst", "rb") as file:
-    readme = file.read().decode("utf-8")
+# 安全读取 README.md，不区分大小写
+def readme():
+    root = Path(__file__).parent
+    readme_file = None
+    for name in os.listdir(root):
+        if name.lower() == "readme.md":
+            readme_file = root / name
+            break
+    if readme_file and readme_file.exists():
+        return readme_file.read_text(encoding="utf-8")
+    return ""  # 文件不存在时返回空字符串
 
 setup(
-    name="loguru",
-    version=version,
-    packages=["loguru"],
-    package_data={"loguru": ["__init__.pyi", "py.typed"]},
-    description="Python logging made (stupidly) simple",
-    long_description=readme,
-    long_description_content_type="text/x-rst",
-    author="Delgan",
-    author_email="delgan.py@gmail.com",
-    url="https://github.com/Delgan/loguru",
-    download_url="https://github.com/Delgan/loguru/archive/{}.tar.gz".format(version),
-    project_urls={
-        "Changelog": "https://github.com/Delgan/loguru/blob/master/CHANGELOG.rst",
-        "Documentation": "https://loguru.readthedocs.io/en/stable/index.html",
+    name='content-hash',
+    description='Python implementation of EIP 1577 content hash',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+
+    version='1.0.0',
+
+    packages=['content_hash'],
+
+    entry_points={
+        'console_scripts': ['content-hash=content_hash.__main__:main'],
     },
-    keywords=["loguru", "logging", "logger", "log"],
-    license="MIT license",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Topic :: System :: Logging",
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Programming Language :: Python :: Implementation :: CPython",
-    ],
+
     install_requires=[
-        "colorama>=0.3.4 ; sys_platform=='win32'",
-        "aiocontextvars>=0.2.0 ; python_version<'3.7'",
-        "win32-setctime>=1.0.0 ; sys_platform=='win32'",
+        'py-cid>=0.3.0,<0.4.0',
+        'py-multicodec>=0.2.1,<0.3.0',
+        'py-multihash>=0.2.3,<0.3.0',
     ],
+
     extras_require={
-        "dev": [
-            "black>=19.10b0 ; python_version>='3.6'",
-            "colorama>=0.3.4",
-            "docutils==0.16",
-            "flake8>=3.7.7",
-            "isort>=5.1.1 ; python_version>='3.6'",
-            "tox>=3.9.0",
-            "pytest>=4.6.2",
-            "pytest-cov>=2.7.1",
-            "Sphinx>=4.1.1 ; python_version>='3.6'",
-            "sphinx-autobuild>=0.7.1 ; python_version>='3.6'",
-            "sphinx-rtd-theme>=0.4.3 ; python_version>='3.6'",
-        ]
+        'lint': ['pylint'],
+        'test': ['pytest', 'pytest-cov'],
     },
-    python_requires=">=3.5",
+
+    python_requires='>= 3.5',
+
+    author='Filip Š',
+    author_email='projects@filips.si',
+    url='https://github.com/filips123/ContentHashPy/',
+    keywords='ethereum, ethereum-name-service, ens, eip1577, web3, decentralized',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: Name Service (DNS)',
+        'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+    ],
+
+    include_package_data=True,
 )
